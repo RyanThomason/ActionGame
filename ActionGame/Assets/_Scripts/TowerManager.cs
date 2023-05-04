c191bae46f5d4745695d8fb493d20cbedcc25ae0
Sajid <sar9727@utulsa.edu>
2023-05-04 00:12:39 -0500

    Added method to rotate to nearest enemy and attack through selected target's script/manager

--- ActionGame/Assets/_Scripts/TowerManager.cs
+++ ActionGame/Assets/_Scripts/TowerManager.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class TowerManager : MonoBehaviour
 {
     [Header("Properties")]
-    public int TowerHealth;
-    public int TowerRange;
-    public int TowerDamage;
+    public float TowerHealth;
+    public float TowerRange = 5f;
+    public float TowerDamage = 10f;
+    public float attackSpeed = 3f;
+    public float rotateSpeed = 10f;
+
+    private float attackTimer = 0f;
+    private Transform target;
     
 
     [Header("Objects")]
@@ -52,13 +57,27 @@ public class TowerManager : MonoBehaviour
         {
             DestroyTower();
         }
-        CurrentTarget = GetNearestTarget();
-        if (CurrentTarget != null)
+        attackTimer += Time.deltaTime;
+        if (attackTimer >= attackSpeed) // determine if the amount of time has passed to attack again
         {
-            
+            CurrentTarget = GetNearestTarget();
+            if (CurrentTarget != null)  // if the CurrentTarget is found; closest one possible, initiate attack sequence
+            {
+                RotateTowardsTarget();
+                if (Vector3.Distance(transform.position, CurrentTarget.transform.position) <= TowerRange)
+                    {
+                        CurrentTarget.GetComponent<EnemyScript>().TakeDamage(TowerDamage);    // Through the script component of the Enemy GameObject, call TakeDamage
+                        attackTimer = 0f;   // reset timer
+                    }
+            }
         }
     }
 
-
+    void RotateTowardsTarget()  // Rotate towards CurrentTarget
+    {
+        Vector3 direction = CurrentTarget.transform.position - transform.position;   
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * rotateSpeed);
+    }
 
 }
