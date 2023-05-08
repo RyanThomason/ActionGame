39077ced8d1407442484a7fe2dbb0616630afb33
RyanThomason <73131611+RyanThomason@users.noreply.github.com>
2023-05-08 03:20:45 -0500

    Player coin fix, scenes, and bullet fixes

--- ActionGame/Assets/_Scripts/PlayerController.cs
+++ ActionGame/Assets/_Scripts/PlayerController.cs
@@ -1,26 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     //Player Variables
     public CharacterController controller;
     public float gravity = -9.81f;
-
     public Transform groundCheck;
     public float groundDistance = 0.2f;
     public LayerMask groundMask;
-
     public float speed = 10f;
-
     Vector3 velocity;
     bool isGrounded;
+    public int coins;
+
+    //Coin text
+    public Text coinTxt;
+
+    void Start() {
+        coins = 300;
+    }
 
 
     // Update is called once per frame
     void Update()
     {
+
+        //set the coin text to update on kill
+        coinTxt.text = "Coins: " + coins.ToString();
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if(isGrounded && velocity.y < 0)
@@ -39,4 +49,7 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
     }
 
+    void AddCoins(int numCoins) {
+        
+    }
 }
